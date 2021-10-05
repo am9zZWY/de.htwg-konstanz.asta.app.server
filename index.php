f9e3f5a9c85ec5ad0a5b7a4b961b8af736804c00
Josef Mueller <j.mueller5021@gmail.com>
2021-10-05 20:49:23 +0200

    🏗️ index.php: can get now information about the print status. - added comments. - added request handling.

--- index.php
+++ index.php
@@ -1,12 +1,79 @@
 <?php
 
-function fetch_and_create_dom($url) {
-    $doc = new DOMDocument();
+/**
+ * Fetch a html from url and return an DOMXPath Object of it.
+ * @param $url
+ * @return DOMXPath
+ */
+function fetch_and_create_dom($url)
+{
     $html = file_get_contents($url);
+    return create_domxpath($html);
+}
+
+/**
+ * Return an DOMXPath Object from a simple html.
+ * @param $html
+ * @return DOMXPath
+ */
+function create_domxpath($html)
+{
+    $doc = new DOMDocument();
     $doc->loadHTML($html);
     return new DOMXPath($doc);
 }
 
+/**
+ * Get information about the Druckerkonto of the HTWG Konstanz.
+ * @param $username
+ * @param $password
+ */
+function get_druckerkonto($username, $password)
+{
+    /* Fields for POST request */
+    $fields = [
+        'username' => $username,
+        'password' => $password,
+        'login' => 'Anmelden'
+    ];
+
+    /**
+     * Initial POST request to log in.
+     * The server then sends back identification cookies which should be used to get the page.
+     */
+    $curl_post_login = curl_init('https://login.rz.htwg-konstanz.de/index.spy');
+    curl_setopt($curl_post_login, CURLOPT_POST, true);
+    curl_setopt($curl_post_login, CURLOPT_POSTFIELDS, http_build_query($fields));
+    curl_setopt($curl_post_login, CURLOPT_HEADER, 1); /* Enable Cookies */
+    curl_setopt($curl_post_login, CURLOPT_RETURNTRANSFER, true); /* Don't dump result; only return it */
+    $result_login = curl_exec($curl_post_login);
+
+    preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result_login, $matches); /* Retrieve cookies and save them to an array */
+    $cookies = array();
+    foreach ($matches[1] as $item) {
+        parse_str($item, $cookie);
+        $cookies = array_merge($cookies, $cookie);
+    }
+
+    /**
+     * Get prepared page from server.
+     * Cookies are needed to authenticate.
+     */
+    $curl_get_druckerkonto = curl_init('https://login.rz.htwg-konstanz.de/userprintacc.spy?activeMenu=Druckerkonto');
+    curl_setopt($curl_get_druckerkonto, CURLOPT_HTTPHEADER, array('Cookie: ' . json_encode($cookies)));
+    curl_setopt($curl_get_druckerkonto, CURLOPT_RETURNTRANSFER, true);
+    $result_druckerkonto = curl_exec($curl_get_druckerkonto);
+
+    /* Get first digits */
+    $matches = array();
+    preg_match('(\d+,\d+)', $result_druckerkonto, $matches);
+
+    echo $matches[0];
+}
+
+/**
+ * Get the newest meals of the HTWG Mensa.
+ */
 function get_speiseplan()
 {
     $xpath = fetch_and_create_dom('https://seezeit.com/essen/speiseplaene/mensa-htwg/');
@@ -38,9 +105,15 @@ function get_speiseplan()
     echo json_encode($speiseplan);
 }
 
+/* Set return-headers to enable CORS policy */
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Max-Age: 86400');
 
-get_speiseplan();
-?>
+
+/* Handle GET requests */
+if (isset($_GET['drucker'])) {
+    echo get_druckerkonto($_GET['username'], $_GET['password']);
+} else if (isset($_GET['mensa']) || isset($_GET['speiseplan'])) {
+    echo get_speiseplan();
+}
